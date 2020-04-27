129fd32e1e3000c0d88472d02f45669440dd2ad7
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-04-27 19:31:55 +0530

    Starting work on navigation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from '../components/Navigation/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <Navigation /> */}
+        <Navigation />
         {/* <Logo />
         <ImageLinkForm />
         <FaceRecognition /> */}
