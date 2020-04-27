a0e8249bea206d97f34c8546a40d793f4bfe86ac
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-04-28 01:56:21 +0530

    Desgin components done. Check particles again.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from '../components/Navigation/Navigation'
+import Particles from 'react-particles-js'
+import Navigation from '../components/Navigation/Navigation';
+import Logo from '../components/Logo/Logo';
+import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
+import Rank from '../components/Rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    }
+  },
+  interactivity: {
+    events: {
+        onhover: {
+            enable: true,
+            mode: "repulse"
+        }
+    }
+}
+}
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Particles className='particles' 
+          param={particlesOptions}
+        />
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
