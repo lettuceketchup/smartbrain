d707823a875a5e598ec0d84e977476418a0fb943
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-04-30 02:23:36 +0530

    Nothing

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -3,7 +3,8 @@ import './Box.css'
 
 const Box = ({ box }) => {
     return (
-        <div className='bounding-box'
+        <div 
+            className='bounding-box'
             style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
         ></div>
     );
