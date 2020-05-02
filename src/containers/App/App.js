e3ea61122bbeb0e89c44e9fbaf28df36834022db
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-05-02 19:57:07 +0530

    Solving empty users problem

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,13 +5,9 @@ import Logo from '../../components/Logo/Logo';
 import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
 import Rank from '../../components/Rank/Rank';
 import FaceRecognition from '../../components/FaceRecognition/FaceRecognition';
-import Clarifai from 'clarifai';
 import SignIn from '../../components/SignIn/SignIn';
 import Register from '../../components/Register/Register';
 import './App.css';
-
-// Instantiate a new Clarifai app by passing in your API key.
-const app = new Clarifai.App({ apiKey: '48ab05d6a0af406586d1179d82f479e8' });
 
 const particlesOptions = {
   particles: {
@@ -33,23 +29,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  boxes: [],
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      boxes: [],
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = data => {
@@ -96,10 +94,14 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+    fetch('http://localhost:3003/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3003/image', {
@@ -113,6 +115,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(err => console.log);
         }
         this.displayFaceBoxes(response)
       })
@@ -121,7 +124,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout' || route === 'signin' || route === 'register')
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     else if (route === 'home')
       this.setState({ isSignedIn: true });
     this.setState({ route: route });
